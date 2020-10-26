9d04a91fc82c2013e1c815d184568f7fbc8d9c8b
juliano acuña <juliano.acunia@hotmail.com>
2020-10-26 20:35:02 -0300

    Exercise five

--- exercise.js
+++ exercise.js
@@ -3,20 +3,26 @@
 var num1 = 2
 var num2 = 2
 var result = num1 + num2
+console.log(result)
 
 // b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 var name = 'Juliano'
 var lastName = 'Acuña'
 var respuesta = name.concat(lastName)
+console.log(respuesta)
 
 // c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
 var name = 'Juliano'
 var lastName = 'Acuña'
 var respuesta = name.length + lastName.length
+console.log(respuesta)
 
 // 2 STRINGS
 // a) Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
 var name = 'juliano jair acuña'
-name.toUpperCase()
+console.log(name.toUpperCase())
 
+// b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+var name = 'Juliano Acuña'
+console.log(name.substring( 0,5))
 
