9cbac4d0d59092700d410193eaa18bd5e9f71fcd
juliano acuña <juliano.acunia@hotmail.com>
2020-10-26 20:25:01 -0300

    Exercise three

--- exercise.js
+++ exercise.js
@@ -2,11 +2,15 @@
 // a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
 var num1 = 2;
 var num2 = 2;
-var respuesta = num1 + num2;
+var result = num1 + num2;
 
 // b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 var name = 'Juliano'
 var lastName = 'Acuña'
-var result = name.concat(name,lastName)
+var respuesta = name.concat(lastName)
 
 // c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+var name = 'Juliano'
+var lastName = 'Acuña'
+var respuesta = name.length + lastName.length
+
