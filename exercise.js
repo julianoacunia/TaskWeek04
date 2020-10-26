790738ae8301230113f886053f8a4d1e3c65eb72
juliano acuña <juliano.acunia@hotmail.com>
2020-10-26 20:07:40 -0300

    Exercise one

---
+++ exercise.js
@@ -0,0 +1,5 @@
+// 1 VARIABLES Y OPERADORES 
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var num1 = Number;
+var num2 = Number;
+var respuesta = num1 + num2;
