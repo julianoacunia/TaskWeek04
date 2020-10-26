4f53eb8c45ed5d1313ac712c6954450f03bc7c1e
juliano acuña <juliano.acunia@hotmail.com>
2020-10-26 20:44:38 -0300

    Exercise seven

--- exercise.js
+++ exercise.js
@@ -32,5 +32,10 @@ var name = 'Juliano Acuña'
 var resultName = name.substring(10,13)
 console.log(resultName)
 
+// d) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+var name = 'juliano acuña'
+var resultName = name.substring(0,1).toUpperCase() + name.substring(1,13).toLowerCase()
+console.log(resultName)
+
 
 
