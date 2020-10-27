67da71ede48eae0731a1fe53e757847267d030f3
juliano acuña <juliano.acunia@hotmail.com>
2020-10-26 21:03:45 -0300

    Exercise ten

--- exercise.js
+++ exercise.js
@@ -48,3 +48,7 @@ var resultName = name.substring(0,1).toUpperCase() + name.substring(1,8).toLower
 console.log(resultName)
 
 // 3 ARRAYS
+// a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log)
+var array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+console.log(array[4],array[10])
+
