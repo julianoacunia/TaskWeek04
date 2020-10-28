2f249147b1c4a369c3774527151b76d1055abb7a
juliano acuña <juliano.acunia@hotmail.com>
2020-10-28 09:30:12 -0300

    exercise eightteen

--- exercise.js
+++ exercise.js
@@ -83,12 +83,29 @@ console.log(array.slice(4,11))
 // 4 If Else
 // a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”
 var num = Math.random()
-
 if( num >= num){ 
     alert('Greater than 0,5')
 }
 else {
     alert('Lower than 0,5')
 }
-
 console.log(num)
+
+// b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+var age = 27;
+if(age < 2) {
+    alert('Bebe')
+} else if(age >= 2 && age <= 12) {
+    alert('Niño')
+} else if(age >= 13 && age <= 19) {
+    alert('Adolescente')
+}else if(age >= 20 && age <= 30) {
+    alert('Joven')
+}else if(age >= 31 && age <= 60) {
+    alert('Adulto')
+}else if(age >= 61 && age <= 75) {
+    alert('Adulto Mayor')
+}else if (age > 75) {
+    alert('Anciano')
+}
+console.log(age)
