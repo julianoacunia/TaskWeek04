de431e5160d8d06625d3afaec905e50f4415e064
juliano acuña <juliano.acunia@hotmail.com>
2020-10-28 09:54:22 -0300

    exercise twenty two

--- exercise.js
+++ exercise.js
@@ -142,3 +142,10 @@ var sentence = ''
 for ( i = 0; i < 1; i++){
     alert(array += sentence)
 }
+
+// d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
+var array = []
+for (i = 0; i < 10; i++){
+    array+=i
+}
+console.log(array)
